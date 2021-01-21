cf90159d133dfe30cefaa41a7b72743d8f5ab28e
Sitaraman <sitaraman.raghav.india@gmail.com>
2021-01-21 21:54:19 +0530

    Route change

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import jsonServer from 'jsonSever';
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router('./genre/Comedy.json');
 const middlewares = jsonServer.defaults();
 const PORT = process.env.PORT || 3000;
 
