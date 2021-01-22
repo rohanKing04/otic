8cdad77d633a2b02c559314d69c6ce234d23f526
Sitaraman <sitaraman.raghav.india@gmail.com>
2021-01-22 18:30:02 +0530

    testing

--- index.js
+++ index.js
@@ -42,21 +42,50 @@ function setResponse(obj) {
     return response;
 }
 
+// app.post('/webhook', (req, res) => {
+//     console.log("Got a post request");
+//     if (!req.body) return res.sendStatus(400);
+//     res.setHeader('Content-Type', 'application/json');
+//     console.log("Post req is :");
+//     console.log(req.body);
+//     console.log("Parameters : " + req.body.queryResult.parameters['genre']);
+//     var genreToSend = req.body.queryResult.parameters['genre'];
+//     for (var genreType in shows) {
+//         if (genreToSend === genreType) {
+//             const resp = setResponse(eval(`shows.${genreType}`)); 
+//             resp.fullfillmentText = `Here are some ${genreType} shows`;
+//             return res.json(resp);
+//         }
+//     }
+// })
+
+
 app.post('/webhook', (req, res) => {
     console.log("Got a post request");
     if (!req.body) return res.sendStatus(400);
-    res.setHeader('Content-Type', 'application/json');
     console.log("Post req is :");
     console.log(req.body);
     console.log("Parameters : " + req.body.queryResult.parameters['genre']);
     var genreToSend = req.body.queryResult.parameters['genre'];
     for (var genreType in shows) {
         if (genreToSend === genreType) {
-            const resp = setResponse(eval(`shows.${genreType}`)); 
-            resp.fullfillmentText = `Here are some ${genreType} shows`;
-            return res.json(resp);
+            var resp = {
+                "fullfillmentMessages": [
+                    {
+                        "text" : {
+                            "text":[
+                                "HEROKU RETURNS",
+                                "HAHAHHA"
+
+                            ]
+                        }
+                    }
+                ]
+            };
+            return res.send(resp);
         }
     }
 })
+
 
 app.listen(port, () => console.log('Server started on port' + port));
